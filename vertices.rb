38269cab118c6efb90297af967e0b83982ecb297
PoirierTheGreat0440E <poirierantoine52@gmail.com>
2026-02-02 22:29:28 +0100

    Further work on vertex listing...

--- vertices.rb
+++ vertices.rb
@@ -1,4 +1,5 @@
 require 'fox16'
+load '3d_calc.rb'
 
 include Fox
 
@@ -16,11 +17,44 @@ end
 
 
 
-class VertexList < FXList
+class Lister < FXHorizontalFrame
+
+  @@icon_vertex = nil
+  @@icon_quad = nil
+  @@icon_texel = nil
 
   def initialize(parent)
-    super(parent,nil,0,LIST_SINGLESELECT|LAYOUT_FILL)
-    @array = [] # We start with an empty vertex array.
+    super(parent,LAYOUT_FILL|PACK_UNIFORM_WIDTH,0,0,10,10,10,10,10,10,10,10)
+    self.setBackColor(FXRGB(200,200,200))
+
+    @@icon_vertex = ItemIcon.new(self.getApp(),"vertex_icon.jpg")
+    @@icon_quad = ItemIcon.new(self.getApp(),"quad_icon.jpg")
+    @@icon_texel = ItemIcon.new(self.getApp(),"texel_icon.jpg")
+
+    @list_vertex = FXList.new(self,nil,0,LAYOUT_FILL|LIST_SINGLESELECT)
+    @list_quad = FXList.new(self,nil,0,LAYOUT_FILL|LIST_SINGLESELECT)
+    @list_texel = FXList.new(self,nil,0,LAYOUT_FILL|LIST_SINGLESELECT)
+
+    @array_vertex = []
+  end
+
+  def refresh_lists()
+    @list_vertex.clearItems()
+    @list_quad.clearItems()
+    @list_texel.clearItems()
+    
+    index = 0
+    @array_vertex.each do |item|
+      @list_vertex.appendItem(item.to_s,@@icon_vertex)
+      @list_quad.appendItem((index/4).to_s,@@icon_quad)
+      @list_texel.appendItem("nothing",@@icon_texel)
+      index = index + 1
+    end
+  end
+
+  def add_vertex(new_vertex)
+    @array_vertex.push(new_vertex)
+    self.refresh_lists()
   end
 
 end
@@ -30,14 +64,20 @@ end
 class MainWindow < FXMainWindow
 
   def initialize(application)
-    @icon1 = ItemIcon.new(application,"vertex_icon.jpg")
-    @icon2 = ItemIcon.new(application,"quad_icon.jpg")
-    @icon3 = ItemIcon.new(application,"texel_icon.jpg")
-    super(application,"VertexList demonstration",nil,nil,DECOR_ALL,0,0,300,300,0,0,0,0,0,0)
-    @vertexlist = VertexList.new(self)
-    @vertexlist.appendItem("test",@icon1)
-    @vertexlist.appendItem("test",@icon2)
-    @vertexlist.appendItem("test",@icon3)
+    super(application,"VertexList demonstration",nil,nil,DECOR_ALL,0,0,700,300,0,0,0,0,0,0)
+    @lister = Lister.new(self)
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
+    @lister.add_vertex(Point.new(1,1,1))
   end
 
 end
